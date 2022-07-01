5b2dcf99c15047957d33288c38bbda152deb1487
WladyslawK <64751494+WladyslawK@users.noreply.github.com>
2022-07-01 15:07:49 +0200

    Update SumOfTwoLowestPositiveIntegers.js

--- SumOfTwoLowestPositiveIntegers.js
+++ SumOfTwoLowestPositiveIntegers.js
@@ -1,32 +1,8 @@
 let numbers = [19, 5, 42, 2, 77];
 
 function sumTwoSmallestNumbers(numbers) {  
-    //Code here
-    let min = numbers[0];
-    let minIndex;
-    let result = 0;
-    let numbersCopy = [];
-  
-  for(let i = 0; i < numbers.length; i++){
-    numbersCopy[i] = numbers[i];
-  }
-  
-  
-    for(let i = 0; i < 2; i++){
-       for(let i = 0; i < numbers.length; i++){
-       if(min > numbersCopy[i]){
-         min = numbersCopy[i];
-         minIndex = i;
-       }
-     } 
-     numbersCopy.splice(minIndex, 1);
-     console.log("After splice: ", numbersCopy)
-     result += min; 
-     min = numbersCopy[0];
-     minIndex = 0;
-    }
-   
-    return result;
+   const [a, b] = numbers.sort((a, b) => a -b);
+    return a + b;
   }
 
   console.log(sumTwoSmallestNumbers(numbers));
