0d9903f3e729d6133879dba72760b237400f4085
WladyslawK <vladkurr7890@gmail.com>
2022-08-04 15:23:04 +0200

    Another way to reverse

--- ReversedList.js
+++ ReversedList.js
@@ -1,1 +1,19 @@
 const reverseList = (arr) => arr.reverse();
+
+const reverseList2= (arr) => {
+    let reversed = [];
+    for(let i = arr.length -1; i >= 0; i--){
+        reversed.push(arr[i]);
+    }
+    return reversed;
+}
+console.log(reverseList2([1,2,3]))
+
+
+function reverseList3(arr) {
+    let res = [];
+    for(let e of arr) {
+      res.unshift(e);
+    }
+    return res;
+  }
