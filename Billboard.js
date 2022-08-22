1543c4234fc17620e3dff81aad36dabe0869ec5e
WladyslawK <vladkurr7890@gmail.com>
2022-08-22 15:39:39 +0200

    Billboard

---
+++ Billboard.js
@@ -0,0 +1,9 @@
+const billboard = (name, price = 30) => {
+    let cost = 0
+    for(let i = 0; i < name.length; i++){
+      cost += price
+    }
+    return cost
+  } 
+  
+  console.log(billboard("Jeong-Ho Aristotelis"))
