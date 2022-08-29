faf17cf0244f7055fc454927428064d6770fe715
WladyslawK <vladkurr7890@gmail.com>
2022-08-29 23:36:31 +0200

    Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!

---
+++ TakeaNumberAndSumItsDigitsRaisedToTheConsecutivePowersAnd.js
@@ -0,0 +1,13 @@
+const sumDigPow = (a, b) => {
+    const array = []
+    for(let i = a; i <= b; i++){
+      let num = i
+      let checkNum = 0
+      let numLength = String(i).length
+      for(let j = 0; j < numLength; j++){
+        checkNum += Math.pow(+String(num)[j], j+1)
+      }
+      if(num===checkNum) array.push(num) 
+    }
+    return array
+}
