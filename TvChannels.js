b91054d84b496d8c6b3d829f7153ca8442fea529
WladyslawK <vladkurr7890@gmail.com>
2023-04-21 23:02:42 +0200

    Tv Channels

--- TvChannels.js
+++ TvChannels.js
@@ -1,5 +1,13 @@
 function redarr(arr) {
-    // your code
+    const result = {}
+      const unique = arr.filter((value, index, array) => array.indexOf(value) === index)
+    .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
     
-    
+    unique.forEach((item, index) => {
+      result[index] = item
+    })
+      
+      console.log(result)
+                  
+      return result
     }
